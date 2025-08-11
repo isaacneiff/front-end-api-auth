043dcd7d815f3e5173b85bdc5ebc09da13399d3e
isaac neiff <isaacalves98017@gmail.com>
2025-08-11 15:18:26 -0300

    subindo com o backend-configurando o railway.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "back-end-auth-token-production.up.railway.app", // backend rodando
+  baseURL: "back-end-auth-token-production.up.railway.app/api", // backend rodando
   headers: {
     "Content-Type": "application/json",
   },
